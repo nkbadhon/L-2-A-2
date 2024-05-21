813776bf0048f062d037546d479cc833e20c2f23
Nk <Badhon7574@gmail.com>
2024-05-21 18:23:20 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
+
+async function main() {
+  // using try catch method to find out error if occurs
+  try {
+    // Connection with mongoDB
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
